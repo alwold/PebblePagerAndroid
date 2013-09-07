03a1415b5e346dfa05fdaaab26da89fa26d3862b
Al Wold <alwold@alwold.com>
2013-09-07 14:54:15 -0700

    ringtone picker works

--- src/com/pebblepager/MainActivity.java
+++ src/com/pebblepager/MainActivity.java
@@ -4,12 +4,15 @@ import java.util.UUID;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.getpebble.android.kit.PebbleKit;
 
@@ -20,19 +23,33 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		PebbleKit.registerReceivedDataHandler(this, new PagerDataReceiver(UUID.fromString("389d91c5-f84c-4fc1-a1a0-baa8ea1b436f"), this));
-		
+		String ringtoneString = getSharedPreferences("PebblePager",  MODE_PRIVATE).getString("ringtone", null);
+		Ringtone ringtone = null;
+		if (ringtoneString != null) {
+			Uri uri = Uri.parse(ringtoneString);
+			ringtone = RingtoneManager.getRingtone(this, uri);
+		}
+		if (ringtone != null) {
+			((TextView) findViewById(R.id.ringtone_label)).setText(ringtone.getTitle(this));
+		}
 		findViewById(R.id.ringtone_button).setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				startActivityForResult(new Intent(RingtoneManager.ACTION_RINGTONE_PICKER), 0);
+				Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+				startActivityForResult(intent, 0);
 			}
 		});
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		Log.d("MainActivity", "picked "+data.getExtras().get(RingtoneManager.EXTRA_RINGTONE_PICKED_URI));
+		Uri uri = (Uri) data.getExtras().get(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+		Editor e = getSharedPreferences("PebblePager", MODE_PRIVATE).edit();
+		e.putString("ringtone", uri.toString());
+		e.commit();
+		Ringtone ringtone = RingtoneManager.getRingtone(this, uri);
+		((TextView) findViewById(R.id.ringtone_label)).setText(ringtone.getTitle(this));
 	}
 
 	@Override
