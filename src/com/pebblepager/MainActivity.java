3b2e4b3a0872c90804bb6f4d19ce1860a3a447dd
Al Wold <alwold@alwold.com>
2013-09-07 14:18:10 -0700

    - add some of the ui for the app - add ringtone picker

--- src/com/pebblepager/MainActivity.java
+++ src/com/pebblepager/MainActivity.java
@@ -2,11 +2,16 @@ package com.pebblepager;
 
 import java.util.UUID;
 
-import com.getpebble.android.kit.PebbleKit;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.getpebble.android.kit.PebbleKit;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +20,19 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		PebbleKit.registerReceivedDataHandler(this, new PagerDataReceiver(UUID.fromString("389d91c5-f84c-4fc1-a1a0-baa8ea1b436f"), this));
+		
+		findViewById(R.id.ringtone_button).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivityForResult(new Intent(RingtoneManager.ACTION_RINGTONE_PICKER), 0);
+			}
+		});
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("MainActivity", "picked "+data.getExtras().get(RingtoneManager.EXTRA_RINGTONE_PICKED_URI));
 	}
 
 	@Override
