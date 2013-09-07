888e08185b210acfbe8d5b332abbc3c8fbbf47e2
Al Wold <alwold@alwold.com>
2013-09-07 15:24:06 -0700

    add support for installing the pebble app

--- src/com/pebblepager/MainActivity.java
+++ src/com/pebblepager/MainActivity.java
@@ -1,14 +1,21 @@
 package com.pebblepager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,6 +24,8 @@ import android.widget.TextView;
 import com.getpebble.android.kit.PebbleKit;
 
 public class MainActivity extends Activity {
+	private static final String PEBBLE_LAUNCH_COMPONENT = "com.getpebble.android";
+	private static final String PEBBLE_LAUNCH_ACTIVITY = "com.getpebble.android.ui.UpdateActivity";
 	private PagerDataReceiver receiver;
 
 	@Override
@@ -42,6 +51,12 @@ public class MainActivity extends Activity {
 				startActivityForResult(intent, 0);
 			}
 		});
+		findViewById(R.id.install_button).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				installApp();
+			}
+		});
 	}
 	
 	@Override
@@ -60,6 +75,56 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void installApp() {
+		 Log.d("MainActivity", "copy_watchface_from_assets()");
+		    InputStream input = null;
+		    OutputStream output = null;
+		    try {
+		        input = getAssets().open("pebble_pager.pbw");
+		        File dest = new File(getExternalCacheDir(), "pebble_pager.pbw");
+		        // delete existing file
+		        dest.delete();
+		        output = new FileOutputStream(dest);
+		        Log.d("MainActivity", "storing pbw: " + dest);
+		        
+		        // copy asset to file
+		        byte[] buffer = new byte[2056];
+		        int length;
+		        while ((length = input.read(buffer))>0){
+		            output.write(buffer, 0, length);
+		        }
+		        output.flush();
+		        
+		        // launch pebble update activity
+		        Intent intent = new Intent(Intent.ACTION_VIEW);
+		        intent.setData(Uri.fromFile(dest));
+		        intent.setClassName(PEBBLE_LAUNCH_COMPONENT, PEBBLE_LAUNCH_ACTIVITY);
+		        Log.d("MainActivity", "launch pebble to load pbw");
+		        try {
+		            startActivity(intent);
+		        } catch (ActivityNotFoundException e) {
+			    	Log.e("MainActivity", "Exception", e);
+		        }
+		    } catch (IOException e) {
+		    	Log.e("MainActivity", "Exception", e);
+		    } finally {
+		        if (input != null) {
+		            try {
+		                input.close();
+		            } catch (IOException e) {
+				    	Log.e("MainActivity", "Exception", e);
+		            }
+		        }
+		        if (output != null) {
+		            try {
+		                output.close();
+		            } catch (IOException e) {
+				    	Log.e("MainActivity", "Exception", e);
+		            }
+		        }
+		    }
 	}
 
 }
